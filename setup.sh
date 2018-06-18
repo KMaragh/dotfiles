d3239e6e630fde635183eacd231ffa0cec5bdd7f
Kadeem M <ksmaragh@gmail.com>
2018-06-17 22:06:57 -0400

    Added print statements.

--- setup.sh
+++ setup.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+echo "Making backup directory"
 mkdir ~/.dotfiles_backup
+
+echo "Moving current dotfiles to backup directory"
 mv ~/.bash_profile ~/.bash_aliases ~/.bashrc ~/.gitconfig ~/.tmux.conf ~/.vim ~/.vimrc ~/.bash_scripts ~/.dotfiles_backup
+
+echo "Symlinking dotfiles directory."
 ln -sf ~/.dotfiles/bash_profile ~/.bash_profile
 ln -sf ~/.dotfiles/bash_aliases ~/.bash_aliases
 ln -sf ~/.dotfiles/bashrc ~/.bashrc
@@ -10,3 +15,6 @@ ln -sf ~/.dotfiles/tmux.conf ~/.tmux.conf
 ln -sf ~/.dotfiles/vim ~/.vim
 ln -sf ~/.dotfiles/vimrc ~/.vimrc
 ln -sf ~/.dotfiles/bash_scripts ~/.bash_scripts
+
+echo "Sourcing extra bash scripts"
+source ~/.bash_scripts/
